7bff6fc2c54f8ae1ab7e656ea0649cac1b39d81b
noisycricket777 <joshbmitchell@gmail.com>
2018-09-04 23:01:56 -0600

    Initial setup test

---
+++ setup-git.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+
+
+
+
+git init
+git config --global user.name "noisycricket777"
+git config --global user.email "joshbmitchell@gmail.com"
+sudo git config --system core.editor nano
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+
+
+
+echo "$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%"
+echo "%$%$%$%$%$%$%$%$%$%$%     THE END     %$%$%$%$%$%$%$%$%$%$%$%$%$"
+echo "$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%"
