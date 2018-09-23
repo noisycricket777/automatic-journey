9c310504babfc201e0e1172f8cd8d64b813fce34
noisycricket777 <joshbmitchell@gmail.com>
2018-09-23 00:08:38 -0600

    Personal Settings first commit

---
+++ Personal/400-install-personal-settings-screenshot-to-jpg-v2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website : https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo "################################################################"
+echo "#########       Screenshot settings             ################"
+echo "################################################################"
+
+echo "Making sure gnome-screenshot saves in jpg - smaller in kb"
+
+cd settings/gnome-screenshot
+sh set-gnome-screenshot-to-save-as-jpg.sh
+
+
+echo "################################################################"
+echo "#########    screenshot settings  installed     ################"
+echo "################################################################"
