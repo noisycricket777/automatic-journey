115009871cf90cc419be67837b1cff62b5d28c0c
noisyricket777 <joshbmitchell@gmail.com>
2018-11-26 19:56:34 -0700

    added scripts for some install 	new file:   000-use-all-cores.sh 	new file:   100-core-software.sh 	new file:   200-software-AUR-repo.sh 	new file:   300-install-fonts.sh 	new file:   400-fix-microcode-error.sh 	modified:   Personal/100-install-personal-settings-folders-v2.sh 	new file:   Personal/settings/fonts/ArchitectsDaughter-Regular.ttf 	new file:   Personal/settings/fonts/BlackOpsOne-Regular.ttf 	new file:   Personal/settings/fonts/Cantarell-Bold.otf 	new file:   Personal/settings/fonts/Cantarell-ExtraBold.otf 	new file:   Personal/settings/fonts/Cantarell-Light.otf

---
+++ 100-core-software.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+#software from Arch Linux repositories
+
+sudo pacman -S --noconfirm --needed screenfetch
+sudo pacman -S --noconfirm --needed dropbox
+sudo pacman -S --noconfirm --needed insync
+
+###############################################################################################
+
+echo "################################################################"
+echo "###################    core software installed  ################"
+echo "################################################################"
---
+++ 200-software-AUR-repo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#set -e
+###########################################################################################################
+
+# software from AUR (Arch User Repositories)
+# https://aur.archlinux.org/packages/
+
+sh AUR/install-insync-v*.sh
+sh AUR/install-numix-folders-git-v*.sh
+sh AUR/install-gpmdp-v*.sh
+sh AUR/install-the-platinum-searcher-bin-v*.sh
+sh AUR/install-sardi-extra-icons-v*.sh
+sh AUR/install-virtualbox-for-linux-v*.sh
+
+# these come last always
+echo "Checking if icons from applications have a hardcoded path"
+echo "and fixing them"
+echo "Wait for it ..."
+sudo hardcode-fixer
+
+echo "################################################################"
+echo "####        Software from AUR Repository installed        ######"
+echo "################################################################"
