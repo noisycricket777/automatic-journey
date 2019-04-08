8008fa026b67c0162dfbfba991ae871bb2302730
noisycricket777 <joshbmitchell@gmail.com>
2019-04-07 22:40:48 -0600

    added some packages

--- 200-software-AUR-repo.sh
+++ 200-software-AUR-repo.sh
@@ -10,14 +10,17 @@ sh AUR/install-neofetch-v*.sh
 sh AUR/install-gpmdp-v*.sh
 sh AUR/install-the-platinum-searcher-bin-v*.sh
 sh AUR/install-dropbox-v*.sh
-#sh AUR/install-virtualbox-for-linux-v*.sh
+sh AUR/install-meld-v*.sh
 sh AUR/install-sublime-text-v*.sh
-sh AUR/install-hardcode-fixer-git-v*.sh
-sh AUR/install-boostnote-v*.sh
+sh AUR/install-joplin-v*.sh
+#sh AUR/install-boostnote-v*.sh
 sh AUR/install-clipgrab-v*.sh
 sh AUR/install-grsync-v*.sh
 sh AUR/install-mkchromecast-v*.sh
 sh AUR/install-grsync-v*.sh
+sh AUR/install-variety-v*.sh
+
+sh strap.sh
 
 
 
