58d98c45db388c5f06d58f4fc096bf7ff6e2ec8c
noisycricket777 <joshbmitchell@gmail.com>
2019-05-17 17:29:30 -0600

    fix bug with vivaldi package and added strah.sh to script

--- 200-software-AUR-repo.sh
+++ 200-software-AUR-repo.sh
@@ -5,6 +5,7 @@
 # software from AUR (Arch User Repositories)
 # https://aur.archlinux.org/packages/
 
+
 sh AUR/install-insync-v*.sh
 sh AUR/install-neofetch-v*.sh
 sh AUR/install-gpmdp-v*.sh
@@ -17,14 +18,10 @@ sh AUR/install-boostnote-v*.sh
 sh AUR/install-clipgrab-v*.sh
 sh AUR/install-grsync-v*.sh
 sh AUR/install-mkchromecast-v*.sh
-sh AUR/install-grsync-v*.sh
 sh AUR/install-grub-customizer-v*.sh
 sh AUR/install-variety-v*.sh
 sh AUR/install-zsh-v*.sh
 sh AUR/install-hardcode-fixer-git-v*.sh
-
-
-
 
 
 # these come last always
