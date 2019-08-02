79d5a1a62bdab2700ca360700f957ee15137f7ef
noisycricket777 <joshbmitchell@gmail.com>
2019-08-02 15:40:03 -0600

    updated name

--- setup-git.sh
+++ setup-git.sh
@@ -6,11 +6,11 @@
 
 
 git init
-    sudo git config --global user.name "noisycricket777"
-    sudo git config --global user.email "joshbmitchell@gmail.com"
-    sudo git config --system user.name "noisycricket777"
-    sudo git config --system user.email "joshbmitchell@gmail.com"
-    sudo git config --system core.editor nano
+    git config --global user.name "noisycricket777"
+    git config --global user.email "joshbmitchell@gmail.com"
+    git config --system user.name "noisycricket777"
+    git config --system user.email "joshbmitchell@gmail.com"
+    git config --system core.editor nano
     git config --global credential.helper cache
     git config --global credential.helper 'cache --timeout=25000'
     git config --global push.default simple
