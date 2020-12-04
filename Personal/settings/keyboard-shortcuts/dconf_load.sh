7416c21d9a2fca7339d7d2d687d25378ef34dee5
Joshua B Mitchell <joshbmitchell@gmail.com>
2020-12-03 17:57:38 -0700

    adde new list

--- Personal/settings/keyboard-shortcuts/dconf_load.sh
+++ Personal/settings/keyboard-shortcuts/dconf_load.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 #
 ##################################################################################################################
-# Author	:	Erik Dubois
-# Website	:	https://www.erikdubois.be
-# Website	:	https://www.arcolinux.info
-# Website	:	https://www.arcolinux.com
-# Website	:	https://www.arcolinuxd.com
-# Website	:	https://www.arcolinuxforum.com
-##################################################################################################################
-
-##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
 #
 ##################################################################################################################
 
-sudo pacman -S dconf dconf-editor --noconfirm --needed
+sudo dnf install -y dconf dconf-editor
 
-dconf load /org/cinnamon/desktop/keybindings/ < keyboard-shortcuts-cinnamon.dconf
+dconf load /org/gnome/shell/keybindings/ < keyboard-shortcuts.dconf
 
 echo "################################################################"
 echo "###################    shortcuts loaded   ######################"
